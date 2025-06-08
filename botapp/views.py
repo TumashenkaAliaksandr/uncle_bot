2cada5561a4bdf1c58ce5c01a36c520621838932
ty4a <tumashenkaaliaksandr@gmail.com>
2025-06-08 19:29:48 +0300

    added new styles css, added model album and tracks on the page

--- botapp/views.py
+++ botapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from botapp.models import Album
+
 
 def index(request):
-    return render(request, 'index.html')
+
+    album = Album.objects.prefetch_related('tracks').first()
+    return render(request, 'index.html', {'album': album})
