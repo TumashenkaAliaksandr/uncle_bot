30fb7cdc10e9c6e903b65e192a0a42335eaddcf4
Admin <tumashenkaaliaksandr@gmail.com>
2025-07-01 14:08:55 +0300

    added new lib - decouple, added to gitignore any files

--- uncle_bot/settings.py
+++ uncle_bot/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+# from django.conf.global_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j%atk@&y)$42v*@=u%obss_9jcl(o)9!)96w$hgk&c4!=pdvxu'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
